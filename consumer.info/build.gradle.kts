e54783b9bceb601572a51239d90a778658347770
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2022-11-10 11:18:52 +0100

    Initialized first consumer (info) project

---
+++ consumer.info/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    id("au.com.dius.pact") version "4.3.16"
+}
+
+group = "swe.dependencies.consumer"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("au.com.dius.pact.consumer:junit5:4.3.16")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.bootRun {
+    jvmArgs = listOf("-Dspring.output.ansi.enabled=ALWAYS")
+}
---
+++ consumer.info/src/main/kotlin/swe/dependencies/consumer/info/Application.kt
@@ -0,0 +1,11 @@
+package swe.dependencies.consumer.info
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
